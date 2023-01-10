bce4ac52b5498b52bfa3aa82809efb0eee32560b
seunghwan <sun1238799@naver.com>
2023-01-11 05:32:23 +0900

    feat: useRef로 image와 input맵핑

--- frontend/src/page/insertImage/index.tsx
+++ frontend/src/page/insertImage/index.tsx
@@ -1,17 +1,29 @@
 import Nav from '@/components/nav';
-import React from 'react';
+import React, { useRef } from 'react';
 import download from '../../assets/download.png';
 import './index.scss';
 const InsertImage = () => {
+  const imageRef = useRef<HTMLInputElement>(null);
+
+  const openFile = () => {
+    imageRef.current?.click();
+  };
+
   return (
     <div className="img-view">
       <Nav />
       <div className="insertImage-view">
         <div className="insert_box">
-          <div className="insert_picture">
+          <div onClick={openFile} className="insert_picture">
             <img className="insert_picture-img" src={download} />
             <p>이곳을 클릭하거나 사진을 올려주세요 !</p>
           </div>
+          <input
+            ref={imageRef}
+            type="file"
+            accept="image/*"
+            className="hidden"
+          ></input>
         </div>
       </div>
     </div>
