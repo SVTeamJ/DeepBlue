c43b73413ea14f87e415b7b3a5959ea2ae770bb5
밍구 <isha3992@gmail.com>
2023-01-12 01:50:48 +0900

    feat: background result page 구성

--- frontend/src/page/result/index.tsx
+++ frontend/src/page/result/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import imgA from '../../assets/fishimg.png';
+import imgB from '../../assets/weedimg.png';
+import imgC from '../../assets/house.png';
 
 const Result = () => {
-  return <div>물고기 결과페이지입니다</div>;
+  return <div>
+    <div className = "h-screen w-full bg-gradient-to-b from-cyan-500 to-cyan-800">
+      <img className = "absolute h-64 w-64 bottom-3" src={imgA} />
+      <img className = "absolute h-64 w-64 -right-10 top-[100px]" src={imgA} />
+      <img className = "absolute h-60 w-60 -bottom-3 right-0" src={imgB} />
+      <img className = "absolute h-30 w-30 right-1 top-0 pr-[150px]" src={imgC}/>
+      <p className="text-2xl font-bold text-right hover:text-end leading-loose whitespace-pre">집으로 가기  </p>
+    </div>
+     
+    </div>;
 };
 
 export default Result;
