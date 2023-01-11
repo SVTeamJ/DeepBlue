470444b13d6d9b4b8f40854e2bf754045ec00d47
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-12 03:49:33 +0900

    feat : 물결 표시 구현

--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -16,12 +16,40 @@ const Main = () => {
           <img className="insert_sun" src={sun}></img>
         </div>
         <div className=''>
-          <div className='insert_title'>강태공 이야기</div>//
+          <div className='insert_title'>강태공 이야기</div>
           <button className='insert_jbutton1'>낚시하러가기</button>
           <button className='insert_jbutton2'>내가 잡은 물고기 보기</button>
           <button className='insert_jbutton2'>물때 확인하기</button>
-          <button className='insert_jbutton2'>랭킹 보기</button>
+          <button className='insert_jbutton2'>랭킹보기</button>
+
         </div>
+        <Wave className='insert_wave1' fill="#68E1FD"
+        paused={false}
+          options={{
+            height: 30,
+            amplitude: 20,
+            speed: 0.10,
+            points: 3
+          }}                                                                                                                                                                                           
+        />
+        <Wave className='insert_wave2' fill="#53B4CA"
+        paused={false}
+          options={{
+            height: 20,
+            amplitude: 30,
+            speed: 0.15,
+            points: 3
+          }}
+        />
+        <Wave className='insert_wave3' fill="#4DA8BC"
+        paused={false}
+          options={{
+            height: 1,
+            amplitude: 20,
+            speed: 0.3,
+            points: 5
+          }} 
+        />
         <img className='insert_fisher_man' src={fisher_man}></img>
       </div>
       <InsertImage />
