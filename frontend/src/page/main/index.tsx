186403cd805399c218b4896c92f9f94a3b0ec17c
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-11 13:46:50 +0900

    feat : 배경색 결정

--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -4,7 +4,12 @@ import InsertImage from '../insertImage';
 const Main = () => {
   return (
     <div>
+<<<<<<< develop
       <div className="h-screen w-full bg-lime-500">메인페이지입니다</div>
+=======
+      <div className="h-screen w-full bg-sky-200">메인페이지입니다</div>
+      <div>안녕</div>
+>>>>>>> feat : 배경색 결정
       <InsertImage />
     </div>
   );
