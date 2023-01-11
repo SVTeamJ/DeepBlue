9542b55de6237e436d423b3ac7365b35fc3a680e
seunghwan <sun1238799@naver.com>
2023-01-11 14:46:36 +0900

    feat: fishList 컴포넌트 기본 레이아웃 틀 제작

---
+++ frontend/src/components/fishList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+const FishList = () => {
+  return (
+    <div className="fishList_view">
+      <div className="fishList_view-encyclopedia"></div>
+    </div>
+  );
+};
+
+export default FishList;
--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import InsertImage from '../insetImage';
-
+import FishList from '../../components/fishList';
 const Main = () => {
   return (
     <div>
       <div className="h-screen w-full bg-lime-500">메인페이지입니다</div>
-      <div>안녕</div>
+      <FishList></FishList>
       <InsertImage />
     </div>
   );
