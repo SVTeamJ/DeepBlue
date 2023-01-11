907a4015123847a8dfd0662ec21e082ea44e847c
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-12 00:31:53 +0900

    feat : 요소 위치 결정

--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import InsertImage from '../insertImage';
+import Wave from 'react-wavify';
+import fisher_man from '@/assets/fisher_man.png';
+import gull from '@/assets/gull.png';
+import sun from '@/assets/sun.png';
+import './index.scss';
 
 const Main = () => {
+
+  <Wave fill="#f79902"
+        paused={false}
+        options={{
+          height: 20,
+          amplitude: 50,
+          speed: 0.45,
+          points: 3
+        }}                                                                                                                                                                                           
+  />
+
+
+
   return (
     <div>
-<<<<<<< develop
-      <div className="h-screen w-full bg-lime-500">메인페이지입니다</div>
-=======
-      <div className="h-screen w-full bg-sky-200">메인페이지입니다</div>
-      <div>안녕</div>
->>>>>>> feat : 배경색 결정
+      <div className="main_background">
+
+        <div className='display w-full h flex flex-row justify-between'>
+          <img className="insert_gull" src={gull}></img>
+          <img className="insert_gull" src={sun}></img>
+        </div>
+        <div className='f'>
+          <button className='insert_jbutton1'>1dddd</button>
+          <button className='insert_jbutton2'>2ddd</button>
+        </div>
+        <img className='insert_fisher_man' src={fisher_man}></img>
+        <Wave className='insert_wave'>
+
+        </Wave>
+      </div>
+
+
+
       <InsertImage />
     </div>
   );
