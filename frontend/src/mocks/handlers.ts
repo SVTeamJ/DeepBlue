915637c69dd7e72ad2656ccc257e8fb2a09e2dc8
seunghwan <sun1238799@naver.com>
2023-01-12 14:49:33 +0900

    feat: 도감데이터 가져오는 몫api구현

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,3 +1,33 @@
 import { rest } from 'msw';
+/**
+ fish type
+ toxicity
+ closed_season
+ description
+ img 
+ */
 
-export const handlers = [];
+interface fishInform {
+  fish_type: string;
+  toxicyty: string;
+  image: string;
+  closed_season: string;
+  description: string;
+}
+
+const fishList: fishInform[] = [
+  {
+    fish_type: '갈치',
+    toxicyty: 'yes',
+    image:
+      'http://health.chosun.com/site/data/img_dir/2015/10/23/2015102301141_0.jpg',
+    closed_season: '3월 22일 ~ 6월 33일',
+    description: '이건 갈치입니다',
+  },
+];
+
+export const handlers = [
+  rest.get('/api/v1/fishList/all', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(fishList));
+  }),
+];
