78c28362f3c2caa649190bed4972eb2f9ac5dfe0
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-12 18:05:06 +0900

    feat : flex 스타일 요소 적용 진행

--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -11,23 +11,23 @@ const Main = () => {
   return (
     <div>
       <div className="main_background">
-        <div className='display w-full flex flex-row justify-between'>
+        <div className='main_left_background'>
           <img className="insert_gull" src={gull}></img>
-          <img className="insert_sun" src={sun}></img>
+          <img className='insert_fisher_man' src={fisher_man}></img>
         </div>
-        <div className=''>
-          <div className='insert_title'>강태공 이야기</div>
-          <button className='insert_jbutton1'>낚시하러가기</button>
-          <button className='insert_jbutton2'>내가 잡은 물고기 보기</button>
-          <button className='insert_jbutton2'>물때 확인하기</button>
-          <button className='insert_jbutton2'>랭킹 보기</button>
 
+        <div className='main_right_background'>
+          <img className="insert_sun" src={sun}></img>
+          <div className='insert_button_map'>
+            <div>강태공 이야기</div>
+            <button >낚시하러가기</button>
+          </div> 
         </div>
         <Wave className='insert_wave1' fill="#68E1FD"
         paused={false}
           options={{
-            height: 30,
-            amplitude: 20,
+            height: 40,
+            amplitude: 40,
             speed: 0.10,
             points: 3
           }}                                                                                                                                                                                           
@@ -35,8 +35,8 @@ const Main = () => {
         <Wave className='insert_wave2' fill="#53B4CA"
         paused={false}
           options={{
-            height: 20,
-            amplitude: 30,
+            height: 40,
+            amplitude: 40,
             speed: 0.15,
             points: 3
           }}
@@ -44,13 +44,12 @@ const Main = () => {
         <Wave className='insert_wave3' fill="#4DA8BC"
         paused={false}
           options={{
-            height: 1,
-            amplitude: 20,
+            height: 40,
+            amplitude: 40,
             speed: 0.3,
             points: 5
           }} 
         />
-        <img className='insert_fisher_man' src={fisher_man}></img>
       </div>
       <InsertImage />
     </div>
