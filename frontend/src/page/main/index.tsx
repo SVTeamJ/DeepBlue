81158c8c5804d39087adfcfee3f7050b631b7305
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-13 01:20:23 +0900

    feat : 낚시하러가기 버튼 기능 구현

--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -8,6 +8,14 @@ import './index.scss';
 
 const Main = () => {
 
+  const goToBottom = () => {
+    window.scrollTo({
+      left : 0,
+      top: document.body.scrollHeight,
+      behavior:"smooth"
+    });
+  };
+
   return (
     <div>
       <div className="main_background">
@@ -15,12 +23,11 @@ const Main = () => {
           <img className="insert_gull" src={gull}></img>
           <img className='insert_fisher_man' src={fisher_man}></img>
         </div>
-
         <div className='main_right_background'>
           <img className="insert_sun" src={sun}></img>
           <div className='insert_button_map'>
             <div className='insert_title'>강태공 이야기</div>
-            <div className='insert_jbutton'> 낚시하러가기 </div>
+            <div onClick={goToBottom} className='insert_jbutton' > 낚시하러가기 </div>
             <div className='insert_jbutton'> 내가 잡은 물고기 보기 </div>
             <div className='insert_jbutton'> 물때 확인 하기 </div>
             <div className='insert_jbutton'> 랭킹 보기 </div>
