896bae4390fcbad41066f6572dafe852f762bc4b
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-13 01:03:04 +0900

    feat : 버튼 반응형 구현

--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -19,11 +19,11 @@ const Main = () => {
         <div className='main_right_background'>
           <img className="insert_sun" src={sun}></img>
           <div className='insert_button_map'>
-            <div>강태공 이야기</div>
+            <div className='insert_title'>강태공 이야기</div>
             <div className='insert_jbutton'> 낚시하러가기 </div>
-            <div className='insert_jbutton'> 낚시하러가기 </div>
-            <div className='insert_jbutton'> 낚시하러가기 </div>
-            <div className='insert_jbutton'> 낚시하러가기 </div>
+            <div className='insert_jbutton'> 내가 잡은 물고기 보기 </div>
+            <div className='insert_jbutton'> 물때 확인 하기 </div>
+            <div className='insert_jbutton'> 랭킹 보기 </div>
           </div>
         </div>
         <Wave className='insert_wave1' fill="#68E1FD"
