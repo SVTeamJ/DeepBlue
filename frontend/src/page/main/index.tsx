b5d8e94525308404d5b1661794d4563d54ec0172
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-14 06:06:01 +0900

    feat : 로그인 페이지 레이아웃 구현

--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -18,21 +18,24 @@ const Main = () => {
   return (
     <div>
       <div className="main_background">
-        <div className="main_left_background">
-          <img className="insert_gull" src={gull}></img>
-          <img className="insert_fisher_man" src={fisher_man}></img>
+        <div className='main_left_background'>
+          {/*<img className="insert_gull" src={gull}></img>*/}
+          <img className='insert_fisher_man' src={fisher_man}></img>
         </div>
-        <div className="main_right_background">
-          <img className="insert_sun" src={sun}></img>
-          <div className="insert_button_map">
-            <div className="insert_title">강태공 이야기</div>
-            <div onClick={goToBottom} className="insert_jbutton">
-              {' '}
-              낚시하러가기{' '}
+        <div className='main_right_background'>
+          {/*<img className="insert_sun" src={sun}></img>*/}
+          <div className='insert_right_center_background'>
+            <div className='insert_login_page'>
+              <div className='insert_login_and_signin'>
+                <a className='insert_login'>aaa</a>
+                <a className='insert_signin'>bbb</a>
+              </div>
             </div>
-            <div className="insert_jbutton"> 내가 잡은 물고기 보기 </div>
-            <div className="insert_jbutton"> 물때 확인 하기 </div>
-            <div className="insert_jbutton"> 랭킹 보기 </div>
+            {/*<div className='insert_title'>강태공 이야기</div>*/}
+            {/*<div onClick={goToBottom} className='insert_jbutton' > 낚시하러가기 </div>*/}
+            {/*div className='insert_jbutton'> 내가 잡은 물고기 보기 </div>*/}
+            {/*<div className='insert_jbutton'> 물때 확인 하기 </div>*/}
+            {/*<div className='insert_jbutton'> 랭킹 보기 </div>*/}
           </div>
         </div>
         <Wave
