ce48c88e221a1868825250c8f0d3062d1245e466
heejung <gimm9567@gmail.com>
2023-01-15 02:10:43 +0900

    feat: 회원가입 기능 추가 및 로그인 기능 수정

--- backend/crud/user_crud.py
+++ backend/crud/user_crud.py
@@ -1,52 +1,56 @@
 from model import User
 from schemas import user_schema
-from fastapi import Response, HTTPException
+from fastapi import Response
 from sqlalchemy.orm import Session
 from starlette.responses import Response
-from starlette.status import HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED
+from starlette.status import HTTP_204_NO_CONTENT
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_user_by_uername(db: Session, username: str):
+    '''
+    ID로 User데이터를 가져와서 비밀번호를 비교하기 위한 메소드
+
+    parameters
+    ----------
+    db: Session
+    username: str
+
+    return
+    ------
+    ID로 사용자 모델 객체 리턴
+    '''
+    return db.query(User).filter(User.username == username).first()
 
 
-# user
-# id로 user 검색
-def get_user_by_token(db: Session, token: str):
-    username = (
-        db.query(User)
-        .filter(User.is_active == True)
-        .filter(User.token == token)
-        .first()
-    )
-    if not username:
-        return Response(status_code=HTTP_401_UNAUTHORIZED)
-    return username
-
-
-# user 생성
 def create_user(db: Session, user: user_schema.UserCreate):
-    existing_user = (
-        db.query(User)
-        .filter(User.username == user.username)
-        .first()
-    )
-    if existing_user:
-        db_user = (
-            db.query(User)
-            .filter(User.id == existing_user.id)
-            .update({"token": user.token})
-        )
-        db.commit()
-        return db.query(User).filter(User.id == existing_user.id).first()
     db_user = User(
         name=user.name,
         username=user.username,
-        password = user.password,
-        token=user.token
+        password = pwd_context.hash(user.password)
     )
     db.add(db_user)
     db.commit()
     return db_user
 
 
-# user 삭제
+def existing_user(db: Session, user: user_schema.UserCreate):
+    '''
+    동일한 ID로 등록된 사용자가 있는지 조회
+
+    parameters
+    ----------
+    db: Session
+    user: user_schema.UserCreate
+
+    return
+    ------
+    ID로 사용자 모델 객체 리턴
+    '''
+    return db.query(User).filter(User.username == user.username).first()
+
+
 def delete_user_by_id(db: Session, user_id: int):
     user = db.query(User).filter(User.id == user_id).update({"is_active": False})
     db.commit()
--- backend/schemas/user_schema.py
+++ backend/schemas/user_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class UserBase(BaseModel): 
@@ -11,16 +11,47 @@ class User(UserBase):
     name: str
     username: str
     password: str
-    token: str
     is_active: bool
 
+
+class Token(BaseModel):
+    '''
+    Token 스키마
+    로그인 API의 출력 항목인 access_token, token_type, username을 속성으로 함
+    '''
+    access_token: str
+    token_type: str
+    username: str
 
 
 class UserCreate(UserBase):
     name: str
-    username : str
+    username: str
     password: str
-    token: str
+    password_check: str
+
+    @validator('password_check')
+    def password_match(cls, v, values):
+        '''
+        password와 password_check가 동일한지 확인
+
+        parameters
+        ----------
+        cls : class 메소드 사용
+        v : password_check
+        values : UserCreate의 속성들이 {변수명: 값} 형태로 전달
+
+        return
+        ------
+        password가 UserCreate에 있고 입력된 password_check의 값이 password와 일치하지 않는다면:
+            "비밀번호가 일치하지 않습니다"
+        일치한다면:
+        password
+        
+        '''
+        if 'password' in values and v != values['password']:
+            raise ValueError("비밀번호가 일치하지 않습니다")
+        return v
 
 
 class UserRead(UserCreate):
