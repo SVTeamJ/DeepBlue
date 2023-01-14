ed69067bce32be488d9f4a393a2003315c4855fc
heejung <gimm9567@gmail.com>
2023-01-15 02:10:43 +0900

    feat: 회원가입 기능 추가 및 로그인 기능 수정

--- backend/model/user.py
+++ backend/model/user.py
@@ -6,22 +6,6 @@ from database import Base
 from sqlalchemy import func
 
 
-# class User(Base):
-#     __tablename__ = "users"
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     user_type = Column(Boolean, nullable=False, default=True) # 사용자와 관리자
-#     name = Column(String(100), index=True)
-#     gender = Column(String(6), index=True)
-#     age_range = Column(String(20), index=True)
-#     phone_num = Column(String(16), index=True)
-#     token = Column(String(255), nullable=False, index=True)
-#     is_active = Column(Boolean, nullable=False, default=True)
-#     created_at = Column(TIMESTAMP, server_default=func.now())
-#     updated_at = Column(
-#         TIMESTAMP, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
-#     )
-
 class User(Base):
     __tablename__ = "users"
 
