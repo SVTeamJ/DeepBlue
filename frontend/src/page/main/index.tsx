5760a1b5329470db746e9d37006a5bbc15fd5dda
seunghwan <sun1238799@naver.com>
2023-01-17 01:36:01 +0900

    feat: introduce ui제작

--- frontend/src/page/main/index.tsx
+++ frontend/src/page/main/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import InsertImage from '../insertImage';
-import Wave from 'react-wavify';
-import fisher_man from '@/assets/fisher_man.png';
-import gull from '@/assets/gull.png';
-import sun from '@/assets/sun.png';
 import './index.scss';
 import Nav from '@/components/nav';
 
 const Main = () => {
   return (
     <div>
+      <Nav></Nav>
       <div className="main_background">
-        <Nav></Nav>
+        <div className="main_center">
+          <div className="main_introduce">
+            <h1>Upload fish Photo!</h1>
+            <p>
+              When you uploading your fish photo,
+              <br /> we'll show you about fish informations.
+            </p>
+          </div>
+          <div className="main_insertPicture"></div>
+        </div>
+        <div className="main_exam-pictures"></div>
       </div>
     </div>
   );
