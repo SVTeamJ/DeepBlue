38d5eb543eda80a971a9269267b946d6ae064707
seunghwan <sun1238799@naver.com>
2023-01-17 03:36:13 +0900

    refactor: 필요없는 데이터삭제 및 라우팅 수정

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,16 +4,15 @@ import GlobalLayout from './page/_layout';
 
 const Main = lazy(() => import('./page/main'));
 const Result = lazy(() => import('./page/result'));
-const FishList = lazy(() => import('./page/fishList'));
+const Storage = lazy(() => import('./page/storage'));
 export const routes = [
   {
     path: '/',
     element: <GlobalLayout />,
     children: [
       { path: '/', element: <Main />, index: true },
-      { path: '/insert', element: <InsertImage></InsertImage>, index: true },
       { path: '/result', element: <Result />, index: true },
-      { path: '/fishList', element: <FishList></FishList>, index: true },
+      { path: '/storage', element: <Storage></Storage>, index: true },
     ],
   },
 ];
