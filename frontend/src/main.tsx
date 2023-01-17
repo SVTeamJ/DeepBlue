c8a7f7297d769a4cbd7409c5f96e6ba83dcd75f6
seunghwan <sun1238799@naver.com>
2023-01-17 17:46:37 +0900

    chore: sentry적용

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,10 +8,18 @@ import App from './App';
 import './index.css';
 import { worker } from './mocks/worker';
 import { getClient } from './queryClient';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import reset from './reset.scss';
 if (import.meta) {
   worker.start();
 }
+
+Sentry.init({
+  dsn: 'https://aaa37d4cc2c242a3a721442b85b43ec2@o4504518618513408.ingest.sentry.io/4504518619496448',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const queryClient = getClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
