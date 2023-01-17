6b2da8f837c6a418827bed1cd76586d6b02047b6
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-17 23:49:53 +0900

    feat 버튼 ON,OFF 표시 구현

--- frontend/src/page/login/is_login.tsx
+++ frontend/src/page/login/is_login.tsx
@@ -13,8 +13,8 @@ function Is_login_check({round=false}){
   return(
     <div className='insert_login_layout' >
       <div className="insert_switch_login">
-        <button className="switch_button" onClick={set_signin}>로그인</button>{/*버튼 작동함*/}
-        <button className="switch_button" onClick={set_login}>회원가입</button>{/*버튼 작동함*/}
+        <button className={`switch_button_${is_login ? 'true' : 'false' }`} onClick={set_signin}>로그인</button>
+        <button className={`switch_button_${is_login ? 'false' : 'true' }`}  onClick={set_login}>회원가입</button>
       </div>
       <Login_page is_login_input={is_login}/>
     </div>
