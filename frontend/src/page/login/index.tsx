0bc58efa65d411723527cc8ba92adb2f970215f1
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-01-18 17:30:52 +0900

    feat : 반응형 수정

--- frontend/src/page/login/index.tsx
+++ frontend/src/page/login/index.tsx
@@ -10,10 +10,6 @@ const Login = () => {
     <div>
       <div className="login_background">
         <div className='login_left_background'>
-          <div className = 'login_logo_background'>
-            <img className='login_logo' src={Icon}></img>
-            <div className='login_web_name'>DeepBlue</div>
-          </div>
           <div className='login_welcome_background' >
             <div className='login_welcome'>Welcome!</div>
             <div className='login_ment'>Please login to use more service</div>
@@ -21,9 +17,8 @@ const Login = () => {
           </div>
         </div>
         <div className='login_right_background'>
-          <div className='insert_login_layout'>
-            <Is_login_check />
-          </div>
+          <button className='mobile_welcome' >Welcome!</button>
+          <Is_login_check/>
         </div>
       </div>
     </div>
--- frontend/src/page/login/loginpage.tsx
+++ frontend/src/page/login/loginpage.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import whale from '@/assets/png_whale.png';
+import whale from '@/assets/png_whale2.jpeg';
 
 const Login_page = (props:any) => {
   const [id, setId] = useState('');
   const [pw,setPw] = useState('');
-
 
   const [button, setButton] = useState(true);
 
@@ -17,78 +16,97 @@ const Login_page = (props:any) => {
   if(!is_login_page){
     return(
       <div className="login_page">
-        <img className="small_whale" src={whale}></img>
-        <div>아이디</div>
-        <input
-          placeholder="아이디를 입력해주세요"
-          id = "id"
-          className="login_input"
-          onChange={e => {
-            setId(e.target.value);
-          }}
-          //onKeyup={changeButton}
-        />
-        <div>비밀번호</div>
-        <input
-          type="password"
-          placeholder = "비밀번호를 입력해주세요"
-          id="password"
-          className="login_input"
-          onChange={e => {
-            setPw(e.target.value);
-          }}
-          onKeyUp={changeButton}
-        />
-        <button className="login_button">
+        <div className="join_us">
           LOGIN
+        </div>
+        <img className="small_whale" src={whale}></img>
+        <div>
+          <div className="login_text">아이디</div>
+          <input
+            placeholder="아이디를 입력해주세요"
+            id = "id"
+            className="login_input"
+            onChange={e => {
+              setId(e.target.value);
+            }}
+            //onKeyup={changeButton}
+          />
+        </div>
+        
+        <div>
+          <div className="login_text">비밀번호</div>
+          <input
+            type="password"
+            placeholder = "비밀번호를 입력해주세요"
+            id="password"
+            className="login_input"
+            onChange={e => {
+              setPw(e.target.value);
+            }}
+            onKeyUp={changeButton}
+        />
+        </div>
+        <button className="login_button">
+          로그인
         </button>
       </div>
     )
   }else{
     return(
       <div className="login_page">
-        <div>이름</div>
-        <input
-          placeholder = "이름을 입력해주세요"
-          id="name"
-          className="login_input"
-          onChange={e => {
-            setPw(e.target.value);
-          }}
-          onKeyUp={changeButton}
-        />
-        <div>아이디</div>
-        <input
-          placeholder="아이디를 입력해주세요"
-          id = "id"
-          className="login_input"
-          onChange={e => {
-            setId(e.target.value);
-          }}
-          //onKeyup={changeButton}
-        />
-        <div>비밀번호</div>
-        <input
-          type="password"
-          placeholder = "영문자,숫자,특수문자, 포함 최소 8~10자"
-          id="password"
-          className="login_input"
-          onChange={e => {
-            setPw(e.target.value);
-          }}
-          onKeyUp={changeButton}
-        />
-        <div>비밀번호 확인</div>
-        <input
-          type="password"
-          placeholder = "비밀번호를 확인해주세요"
-          id="password"
-          className="login_input"
-          onChange={e => {
-            setPw(e.target.value);
-          }}
-          onKeyUp={changeButton}
-        />
+        <div className="join_us">
+          JOIN US
+        </div>
+        <div>
+          <div className="login_text">이름</div>
+          <input
+            placeholder = "이름을 입력해주세요"
+            id="name"
+            className="login_input"
+            onChange={e => {
+              setPw(e.target.value);
+            }}
+            onKeyUp={changeButton}
+          />
+        </div>
+        <div>
+          <div className="login_text">아이디</div>
+          <input
+            placeholder="아이디를 입력해주세요"
+            id = "id"
+            className="login_input"
+            onChange={e => {
+              setId(e.target.value);
+            }}
+            //onKeyup={changeButton}
+          />
+        </div>
+        <div>
+          <div className="login_text">비밀번호</div>
+          <input
+            type="password"
+            placeholder = "영문자,숫자,특수문자, 포함 최소 8~10자"
+            id="password"
+            className="login_input"
+            onChange={e => {
+              setPw(e.target.value);
+            }}
+            onKeyUp={changeButton}
+          />
+        </div>
+        <div>
+          <div className="login_text">비밀번호 확인</div>
+          <input
+            type="password"
+            placeholder = "비밀번호를 확인해주세요"
+            id="password"
+            className="login_input"
+            onChange={e => {
+              setPw(e.target.value);
+            }}
+            onKeyUp={changeButton}
+          />
+        </div>
         <button className="login_button">
           회원가입
         </button>
