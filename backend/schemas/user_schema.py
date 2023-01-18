8bc7c6f6138c5f2605ef13581af90e4a4b6a7cb8
heejung <gimm9567@gmail.com>
2023-01-19 04:42:16 +0900

    feat: user_schema 작성

--- backend/schemas/user_schema.py
+++ backend/schemas/user_schema.py
@@ -1,62 +1,61 @@
 from pydantic import BaseModel, validator
 
 
-class UserBase(BaseModel): 
-    class Config:
-        orm_mode = True
+class UserBase(BaseModel):
+    name: str
+    username: str
 
 
 class User(UserBase):
     id: int
-    name: str
-    username: str
-    password: str
     is_active: bool
+    password: str
 
-
-class Token(BaseModel):
-    '''
-    Token 스키마
-    로그인 API의 출력 항목인 access_token, token_type, username을 속성으로 함
-    '''
-    access_token: str
-    token_type: str
-    username: str
+    class Config:
+        orm_mode = True
 
 
 class UserCreate(UserBase):
     name: str
-    username: str
-    password: str
+    password1: str
     password_check: str
+
+    @validator('name', 'username', 'password1', 'password_check')
+    def not_empty(cls, v):
+        '''
+        name, username, password1, password_check에 빈 칸이 있는지 확인
+
+        parameter
+        ---------
+        값
+
+        return
+        ------
+        없으면:
+            ValueError
+        있으면:
+            값 반환
+        '''
+        if not v:
+            raise ValueError("모든 값을 입력해야 합니다.")
+        return v
 
     @validator('password_check')
     def password_match(cls, v, values):
         '''
-        password와 password_check가 동일한지 확인
+        password1과 password1이 일치하는지 확인
 
-        parameters
-        ----------
-        cls : class 메소드 사용
-        v : password_check
-        values : UserCreate의 속성들이 {변수명: 값} 형태로 전달
+        parameter
+        ---------
+        값
 
         return
         ------
-        password가 UserCreate에 있고 입력된 password_check의 값이 password와 일치하지 않는다면:
-            "비밀번호가 일치하지 않습니다"
-        일치한다면:
-        password
-        
+        일치하지 않으면:
+            ValueError
+        일치하면:
+            값 반환
         '''
-        if 'password' in values and v != values['password']:
-            raise ValueError("비밀번호가 일치하지 않습니다")
+        if 'password1' in values and v != values['password1']:
+            raise ValueError("비밀번호가 일치하지 않습니다.")
         return v
-
-
-class UserRead(UserCreate):
-    id: int
-
-
-class UserDelete(UserBase):
-    is_active: bool
