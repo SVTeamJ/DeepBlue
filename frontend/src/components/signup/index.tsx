f8de0b2d397b85ff6ddc6d80404c8de745a3ff24
seunghwan <sun1238799@naver.com>
2023-01-22 20:48:20 +0900

    feat: 회원가입 api연동

--- frontend/src/components/signup/index.tsx
+++ frontend/src/components/signup/index.tsx
@@ -1,14 +1,49 @@
+import { restFetcher } from '@/queryClient';
+import { useMutation } from '@tanstack/react-query';
 import React, { useState } from 'react';
 import './index.scss';
 const SignUpComponents = () => {
+  const [userName, setUserName] = useState('');
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
+  const [checkPw, setCheckPw] = useState('');
 
   const [button, setButton] = useState(true);
 
   function changeButton() {
     id.includes('@') && pw.length >= 5 ? setButton(false) : setButton(true);
   }
+
+  //유저생성
+  interface User {
+    name: string;
+    username: string;
+    password1: string;
+    password_check: string;
+  }
+
+  const { mutate, isLoading } = useMutation((newUser: User) => {
+    return restFetcher({
+      method: 'POST',
+      path: 'http://localhost:8000/api/users/signup',
+      body: newUser,
+    });
+  });
+
+  const createUser = () => {
+    const newUser = {
+      name: userName,
+      username: id,
+      password1: pw,
+      password_check: checkPw,
+    };
+    mutate(newUser, {
+      onSuccess: (data) => {
+        console.log(data);
+        alert('회원가입 완료!');
+      },
+    });
+  };
   return (
     <div className="signup_page">
       <div className="join_us">JOIN US</div>
@@ -17,9 +52,10 @@ const SignUpComponents = () => {
         <input
           placeholder="이름을 입력해주세요"
           id="name"
+          required
           className="signup_page_input"
           onChange={(e) => {
-            setPw(e.target.value);
+            setUserName(e.target.value);
           }}
           onKeyUp={changeButton}
         />
@@ -27,6 +63,7 @@ const SignUpComponents = () => {
       <div>
         <div className="signup_page_text">아이디</div>
         <input
+          required
           placeholder="아이디를 입력해주세요"
           id="id"
           className="signup_page_input"
@@ -39,6 +76,7 @@ const SignUpComponents = () => {
       <div>
         <div className="signup_page_text">비밀번호</div>
         <input
+          required
           type="password"
           placeholder="영문자,숫자,특수문자, 포함 최소 8~10자"
           id="password"
@@ -52,17 +90,20 @@ const SignUpComponents = () => {
       <div>
         <div className="signup_page_text">비밀번호 확인</div>
         <input
+          required
           type="password"
           placeholder="비밀번호를 확인해주세요"
           id="password"
           className="signup_page_input"
           onChange={(e) => {
-            setPw(e.target.value);
+            setCheckPw(e.target.value);
           }}
           onKeyUp={changeButton}
         />
       </div>
-      <button className="signup_page_button">회원가입</button>
+      <button onClick={createUser} className="signup_page_button">
+        회원가입
+      </button>
     </div>
   );
 };
