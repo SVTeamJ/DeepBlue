5935a09c115dfea9f6b1b0a29ef78cb701ca012f
seunghwan <sun1238799@naver.com>
2023-01-22 18:21:45 +0900

    fix: any => 타입지정

--- frontend/src/page/login/loginpage.tsx
+++ frontend/src/page/login/loginpage.tsx
@@ -3,17 +3,11 @@ import whale from '@/assets/png_whale2.jpeg';
 import LoginComponent from '@/components/login';
 import SignUpComponents from '@/components/signup';
 
-const Login_page = (props: any) => {
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
-
-  const [button, setButton] = useState(true);
-
-  function changeButton() {
-    id.includes('@') && pw.length >= 5 ? setButton(false) : setButton(true);
-  }
-
-  const is_login_page = props.is_login_input;
+interface Props {
+  is_login: boolean;
+}
+const Login_page = ({ is_login }: Props) => {
+  const is_login_page = is_login;
 
   if (!is_login_page) {
     return <LoginComponent />;
