92193217bf6ddd6a7197cd668335f15473a01c87
kckoh <kyungcheolkoh@gmail.com>
2023-01-22 12:18:12 -0800

    fix: fish 모데 수정

--- backend/model/fish.py
+++ backend/model/fish.py
@@ -9,14 +9,20 @@ class Fish(Base):
     __tablename__="fish"
     
     fish_id=Column(Integer,primary_key=True,index=True)
+
     fish_type=Column(String(64),nullable=False,index=True)
+    scientific_name=Column(String(128),nullable=False,index=True)
+    classification = Column(String(64), nullable=False, index=True)
+    description = Column(String(512), nullable=False, index=False)
+    habitat = Column(String(64), nullable=False, index=False)
+
     toxicity=Column(String(16),nullable=False,index=True)
-    open_season=Column(String(64),nullable=False,index=True)
-    closed_season=Column(String(64),nullable=False,index=True)
-    description=Column(String(255),nullable=False,index=True)
-    fish_url=Column(String(100),nullable=False,index=True)
+    open_season=Column(String(64),nullable=False,index=False)
+    closed_season=Column(String(64),nullable=False,index=False)
+    fish_url=Column(String(100),nullable=False,index=False)
+
     created_at=Column(TIMESTAMP,server_default=func.now())
     updated_at=Column(TIMESTAMP,server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
-    is_active=Column(Boolean,nullable=False,default=True)
+    is_active=Column(Boolean,nullable=False,default=False)
     
   
