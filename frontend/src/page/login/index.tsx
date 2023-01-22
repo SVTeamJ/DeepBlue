f2b5382187d6cb2eeefe7d67c45dbdeaef18633c
seunghwan <sun1238799@naver.com>
2023-01-22 18:56:21 +0900

    refactor: 토글버튼 수정

--- frontend/src/page/login/index.tsx
+++ frontend/src/page/login/index.tsx
@@ -4,12 +4,23 @@ import Is_login_check from './is_login';
 import Icon from '@/assets/icon.png';
 import Whale from '@/assets/whale.png';
 import Nav from '@/components/nav';
+import Login_page from './loginpage';
 
 const Login = () => {
   return (
     <>
       <div className="login_background">
         <Nav />
+        <div className="login_mainLayout">
+          <div className="login_explanation">
+            <h1>Welcome!</h1>
+            <p>Please log in to use more services</p>
+            <img src={Whale}></img>
+          </div>
+          <div className="login_input">
+            <Is_login_check></Is_login_check>
+          </div>
+        </div>
       </div>
     </>
   );
--- frontend/src/page/login/is_login.tsx
+++ frontend/src/page/login/is_login.tsx
@@ -1,31 +1,34 @@
 import React, { useState } from 'react';
 import Login_page from './loginpage';
-
+import './index.scss';
 function Is_login_check({ round = false }) {
   const [is_login, set_is_login] = useState(false);
-  const set_login = () => {
-    set_is_login(true);
+
+  const set_toggle = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.currentTarget.innerHTML == '로그인') {
+      set_is_login(true);
+    } else {
+      set_is_login(false);
+    }
   };
-  const set_signin = () => {
-    set_is_login(false);
-  };
+
   return (
     <div className="insert_login_layout">
       <div className="insert_switch_login">
         <button
           className={`switch_button_${is_login ? 'true' : 'false'}`}
-          onClick={set_signin}
+          onClick={set_toggle}
         >
           로그인
         </button>
         <button
           className={`switch_button_${is_login ? 'false' : 'true'}`}
-          onClick={set_login}
+          onClick={set_toggle}
         >
           회원가입
         </button>
       </div>
-      <Login_page is_login_input={is_login} />
+      <Login_page is_login={is_login} />
     </div>
   );
 }
