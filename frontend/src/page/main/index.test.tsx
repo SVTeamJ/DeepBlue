ae3315f5783ac006ac5924135db21d70b0c675cb
seunghwan <sun1238799@naver.com>
2023-01-25 01:33:45 +0900

    chore: 테스트환경 셋업

---
+++ frontend/src/page/main/index.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import Main from './';
+
+describe('App', () => {
+  it('should have headline', () => {
+    expect(1).toBe(1);
+  });
+});
+
+test('테스트 설명', () => {
+  expect(2).toBe(2);
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+  },
   server: {
     port: 3000,
-    host:true,
+    host: true,
   },
   resolve: {
     alias: {
