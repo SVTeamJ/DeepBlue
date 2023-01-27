11c29bc29ccb19dbf995c95dc49921ee9f855340
seunghwan <sun1238799@naver.com>
2023-01-28 07:44:25 +0900

    feat: 차트 ui 제작

---
+++ frontend/src/page/chart/index.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import './index.scss';
+import ApexChart from 'react-apexcharts';
+import Nav from '@/components/nav';
+import line from '../../assets/Bar.png';
+import circle from '../../assets/circle.png';
+import inline from '../../assets/inline.png';
+import bar from '../../assets/bar.png';
+type chartType = 'line' | 'area' | 'bar' | 'radar' | undefined;
+
+const ChartPage = () => {
+  const [chartType, setChartType] = useState<chartType>('line');
+  const [windowSize, setWindow] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const changeButton = (e: any) => {
+    console.log(e.target.className);
+    setChartType(() => e.target.className);
+  };
+
+  const handleResize = () => {
+    setWindow({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowSize]);
+
+  return (
+    <div className="chart_background">
+      <Nav />
+      <div className="chart_main">
+        <div className="buttons" onClick={changeButton}>
+          <img src={line} className="line"></img>
+          <img src={circle} className="radar"></img>
+          <img src={inline} className="area"></img>
+          <img src={bar} className="bar"></img>
+        </div>
+        <div className="chart">
+          <ApexChart
+            type={chartType}
+            width={
+              windowSize.width < 500
+                ? windowSize.width - 50
+                : windowSize.width / 2.5
+            }
+            height={windowSize.height / 3}
+            series={[
+              {
+                name: '어류들',
+                data: [32, 54, 43, 15, 53],
+              },
+            ]}
+            options={{
+              theme: {
+                mode: 'dark',
+              },
+              chart: {
+                toolbar: {
+                  show: false,
+                },
+                background: 'transparent',
+              },
+              colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
+              grid: { show: false },
+              stroke: {
+                curve: 'smooth',
+                width: 1,
+              },
+              yaxis: {},
+              xaxis: {
+                axisBorder: { show: false },
+                axisTicks: { show: false },
+                labels: {},
+                categories: [
+                  '넙치',
+                  '고등어',
+                  '노랑가오리',
+                  '적색퉁돔',
+                  '갈치',
+                ],
+              },
+
+              fill: {
+                type: 'gradient',
+                gradient: { gradientToColors: ['#0be881'], stops: [0, 100] },
+              },
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartPage;
