435c5037182c7bdabc89bfb01ba64f0b10894f9f
seunghwan <sun1238799@naver.com>
2023-01-29 00:53:44 +0900

    feat: 차트페이지 반응형및 애니메이션 추가

--- frontend/src/page/chart/index.tsx
+++ frontend/src/page/chart/index.tsx
@@ -43,66 +43,77 @@ const ChartPage = () => {
     <div className="chart_background">
       <Nav />
       <div className="chart_main">
-        <div className="buttons" onClick={changeButton}>
-          <img src={line} className="line"></img>
-          <img src={inline} className="area"></img>
-          <img src={circle} className="radar"></img>
-          <img src={bar} className="bar"></img>
+        <div className="chart_text">
+          <h1>This is the fish statistics page</h1>
+          <div>
+            <div>
+              <div>You can visually see the fish</div>
+              <div>that many users have been curious about!</div>
+            </div>
+          </div>
         </div>
-        <div className="chart">
-          <ApexChart
-            type={chartType}
-            width={
-              windowSize.width < 500
-                ? windowSize.width - 50
-                : windowSize.width / 2.5
-            }
-            height={windowSize.height / 3}
-            series={[
-              {
-                name: '어류들',
-                data: [32, 54, 42, 59, 53],
-              },
-            ]}
-            options={{
-              theme: {
-                mode: 'dark',
-              },
-              chart: {
-                toolbar: {
-                  show: false,
+        <div>
+          <div className="buttons" onClick={changeButton}>
+            <img src={line} className="line"></img>
+            <img src={inline} className="area"></img>
+            <img src={circle} className="radar"></img>
+            <img src={bar} className="bar"></img>
+          </div>
+          <div className="chart">
+            <ApexChart
+              type={chartType}
+              width={
+                windowSize.width < 500
+                  ? windowSize.width - 50
+                  : windowSize.width / 2.5
+              }
+              height={windowSize.height / 3}
+              series={[
+                {
+                  name: '어류들',
+                  data: [32, 54, 42, 59, 53],
                 },
-                zoom: {
-                  enabled: false,
+              ]}
+              options={{
+                theme: {
+                  mode: 'dark',
                 },
-                background: 'transparent',
-              },
-              colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
-              grid: { show: false },
-              stroke: {
-                curve: 'smooth',
-                width: 1,
-              },
-              yaxis: {},
-              xaxis: {
-                axisBorder: { show: false },
-                axisTicks: { show: false },
-                labels: {},
-                categories: [
-                  '넙치',
-                  '고등어',
-                  '노랑가오리',
-                  '적색퉁돔',
-                  '갈치',
-                ],
-              },
+                chart: {
+                  toolbar: {
+                    show: false,
+                  },
+                  zoom: {
+                    enabled: false,
+                  },
+                  background: 'transparent',
+                },
+                colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
+                grid: { show: false },
+                stroke: {
+                  curve: 'smooth',
+                  width: 1,
+                },
+                yaxis: {},
+                xaxis: {
+                  axisBorder: { show: false },
+                  axisTicks: { show: false },
+                  labels: {},
+                  categories: [
+                    '넙치',
+                    '고등어',
+                    '노랑가오리',
+                    '적색퉁돔',
+                    '갈치',
+                  ],
+                },
 
-              fill: {
-                type: 'gradient',
-                gradient: { gradientToColors: ['#0be881'], stops: [0, 100] },
-              },
-            }}
-          />
+                fill: {
+                  type: 'gradient',
+                  gradient: { gradientToColors: ['#0be881'], stops: [0, 100] },
+                },
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
