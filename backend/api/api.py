debfd73c4e565e36bf7ce61d5ecebb876d326917
heejung <gimm9567@gmail.com>
2023-02-02 15:06:34 +0900

    refactor: AI API 라우터 수정

--- backend/api/api.py
+++ backend/api/api.py
@@ -3,7 +3,7 @@ from api.endpoints import users, ai,fish,history, charts
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(ai.router, prefix="/ai", tags=["ai"])
+api_router.include_router(ai.router, prefix="/analysis", tags=["analysis"])
 api_router.include_router(history.router, prefix="/history",tags=["history"])
 api_router.include_router(fish.router, prefix="/fish",tags=["fish"])
 api_router.include_router(charts.router, prefix="/charts",tags=["charts"])
